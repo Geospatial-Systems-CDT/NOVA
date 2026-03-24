df7a45357b20c7a15f668e764c452cc12276b792
kaitlynri <kaitlynries@email.com>
2026-03-24 11:48:03 +0000

    Using reveal

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,12 +1,26 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from '../theme';
+import './ReportPage.css';
+import { Deck, Slide } from '@revealjs/react';
+import 'reveal.js/reveal.css';
+import 'reveal.js/theme/black.css';
 
 const ReportPage = () => {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <h1>Report Page</h1>
+        <Deck>
+            <Slide>
+                <h1>Hello</h1>
+                <p>My first Reveal deck in React.</p>
+            </Slide>
+            <Slide background="#111827">
+                <h2>Second slide</h2>
+            </Slide>
+        </Deck>            
         </ThemeProvider>
+
     );
 };
 
