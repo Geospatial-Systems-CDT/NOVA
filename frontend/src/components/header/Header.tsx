31f2ec394043448d1d686580aeacd359d2eb5049
Michael Jones <michael.jones@evolution-apps.co.uk>
2026-03-24 11:20:37 +0000

    remove test  button

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // © Crown Copyright 2026. This work has been developed by the National Digital Twin Programme and is legally attributed to the Department for Business and Trade (UK) as the governing entity.
 
-import { AppBar, Box, Button, styled } from '@mui/material';
+import { AppBar, Box, styled } from '@mui/material';
 import React from 'react';
 import novaLogo from '../../assets/nova-logo.svg';
 import UserMenu from './UserMenu';
@@ -24,15 +24,6 @@ const Header: React.FC<HeaderProps> = ({ onOpenPrivacy }) => {
     return (
         <Toolbar position="relative">
             <Box component="img" src={novaLogo} alt="NOVA Logo" sx={{ height: '1.5rem' }} />
-            {/* TODO: remove temporary test button */}
-            <Button
-                variant="outlined"
-                size="small"
-                sx={{ color: 'white', borderColor: 'white' }}
-                onClick={() => window.open('/report', '_blank', 'noopener,noreferrer')}
-            >
-                Test Report Page
-            </Button>
             <UserMenu onOpenPrivacy={onOpenPrivacy} />
         </Toolbar>
     );
